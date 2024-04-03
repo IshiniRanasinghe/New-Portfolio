163cce8f7f654d8d9a87392a01b0c8c7d532d615
Ishini Ranasinghe <124230210+IshiniRanasinghe@users.noreply.github.com>
2024-04-03 09:25:53 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,274 @@
+export const Bio = {
+  name: "Ishini Ranasinghe",
+  roles: [
+    "Undergraduate Student",
+
+  ],
+  description:
+    "I'm an inspired programmer at a university studying software engineering with a strong interest in original solutions. Focused on lifelong learning and knowledgeable in a few programming languages. A track record of collaborating on projects with people, solving complex problems, and adjusting to rapidly evolving technologies.I am excited to contribute my skills and enthusiasm to an uniqueteam.",
+  github: "https://github.com/IshiniRanasinghe",
+  resume:
+    "https://drive.google.com/drive/folders/1hZT-tZtgi6ryuu_MLzc7_UrbySYp3KVr",
+  linkedin: "https://www.linkedin.com/in/ishini-ranasinghe-76589a257/",
+  twitter: "",
+  instagram: "https://www.instagram.com/_i_s_h_16/",
+  facebook: "https://www.facebook.com/profile.php?id=100081182855248",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      
+      
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      
+      
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/ishiniransinghe-portfolio.appspot.com/o/download.png?alt=media&token=a312bd65-02c5-46e8-8a09-64978ff13125",
+    school: "Informatics Institute of Technology (IIT)",
+    date: "2022-2026",
+    grade: "1 st year GPA - 79%",
+    desc: "2nd year =>  Object oriented programming: 85% , Database Systems : 92%",
+    degree: "ICSC(X), Science with Computer",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/ishiniransinghe-portfolio.appspot.com/o/download.jpg?alt=media&token=3b36f0b0-f37c-4b04-8b91-f98c5aae9510",
+    school: "Rathnavali Balika Vidyalaya,Gampaha",
+    date: " 2021 - 2022",
+    grade: "8 A passes and 1 B pass",
+    desc: " Mathematics - C , Physics - C , Chemistry - S , General English - B ",
+    degree: "GCE Advanced level Examination",
+
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/ishiniransinghe-portfolio.appspot.com/o/download.jpg?alt=media&token=3b36f0b0-f37c-4b04-8b91-f98c5aae9510",
+    school: "Rathnavali Balika Vidyalaya,Gampaha",
+    date: "2018",
+    grade: "8 A passes and 1 B pass",
+    desc: "Buddhism - Pass A , Sinhala - Pass A ,Mathematics - Pass A ,Science - Pass A , History - Pass A , Health - Pass A ,Sinhala lit. - Pass A , Tamil - Pass B",
+    degree: "GCE Ordinary level Examination", 
+  },
+];
+
+export const projects = [
+ 
+  
+ 
+  {
+    id: 8,
+    title: "Pet care website",
+    date: "jan 2024",
+    description:
+      "This is the Software Development group project (SDGP) for the second year. The primary feature is the meal recommendations based on the unique tastes of dogs [used ML].",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/ishiniransinghe-portfolio.appspot.com/o/0329(1).gif?alt=media&token=7224264f-b1f7-48a2-b777-8ea9cfd179cc",
+    tags: ["Python", "Java", "JavaScript", "React", "Css", "Machine Learing"],
+    category: "machine learning",
+    github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+    webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  },
+ 
+  {
+    id: 7,
+    title: "Quiz Game",
+    date: "2023",
+    description:
+      "This is my individual part of web design and Development module's group project.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/ishiniransinghe-portfolio.appspot.com/o/Quizz.gif?alt=media&token=2023e127-8d37-4e5b-b3e9-a02672463215",
+    tags: ["HTML", "CSS"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Quiz-Earn",
+    webapp: "https://github.com/rishavchanda/Quiz-Earn",
+  },
+  {
+    id: 8,
+    title: "Bill Generator project  ",
+    date: "2024",
+    description:
+      "individual java project about burger shop's bill generator.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/ishiniransinghe-portfolio.appspot.com/o/BurgerBillGenerator2024-03-2914-53-50-ezgif.com-video-to-gif-converter.gif?alt=media&token=59f5627b-5c95-409a-9642-ab1590893dd7",
+    tags: ["java", "java Swing"],
+    category: "Java Projects",
+    github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+    webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  },
+  {
+    id: 4,
+    title: "Theatre Seat Management Program ",
+    date: "2023",
+    description:
+    "Developed a Java program for New Theatre to manage seat sales and availability during theatre sessions. Features a user-friendly menu for buying tickets, canceling tickets, displaying available seats, and more. Utilized arrays, classes,and objects for efficient data management. Tested thoroughly and adhered to good coding style for clarity and maintainability",
+    image:"https://firebasestorage.googleapis.com/v0/b/ishiniransinghe-portfolio.appspot.com/o/Theatre.png?alt=media&token=abc0e9d2-6dfc-47da-b8ea-123669dba232",
+      tags: ["java", "javaSwing"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Quiz-Earn",
+    webapp: "https://github.com/rishavchanda/Quiz-Earn",
+  },
+  {
+    id: 7,
+    title: "Products Management System",
+    date: "2023",
+    description:
+      "This is University course work for 2nd year.used object-oriented programming to create a Java software that mimics a system for purchasing products. A Java Swing-built graphical user interface is used for interaction",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/ishiniransinghe-portfolio.appspot.com/o/product.png?alt=media&token=b734e2ff-9cb6-43ed-a62e-9621501b8264",
+    tags: [ "Java","Java Swing","OOP","Event handling"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Quiz-Earn",
+    webapp: "https://github.com/rishavchanda/Quiz-Earn",
+  },
+  {
+    id: 7,
+    title: "Progression Outcome Prediction Program",
+    date: "2023",
+    description:
+      "This is University course work for 1st year.Developed a Python program for university coursework to predict student progression outcomes based on credit levels at pass, defer, and fail. Utilized user-defined functions, robust validation, and efficient conditional statements to ensure accuracy. Allows for multiple outcome predictions and generates a concise histogram summary.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/ishiniransinghe-portfolio.appspot.com/o/python.png?alt=media&token=b5cfb4d7-c164-4c6a-bfb9-3e5fc19cde9b",
+    tags: [ "Java","Java Swing","OOP","Event handling"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Quiz-Earn",
+    webapp: "https://github.com/rishavchanda/Quiz-Earn",
+  },
+
+
+];
+
+export const TimeLineData = [
+  { year: 2022, text: "Started to study in " },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
---
+++ src/themes/default.js
@@ -0,0 +1,26 @@
+export default {
+    // Temp fonts
+    fonts: {
+      title: "Space Grotesk, sans-serif",
+      main: "Space Grotesk, sans-serif"
+    },
+    // Colors for layout
+    colors: {
+      primary1: "#45b2d6",
+      background1: "#331c4d",
+      button: "#564cb1",
+      background2: "#13077e",
+      text: "#9fb8d9",
+      text1: "#F2F5F7",
+      text2: "#626970",
+      text3: "#575C66",
+      footerBackground: "#040b4b"
+    },
+    // Breakpoints for responsive design
+    breakpoints: {
+      sm: 'screen and (max-width: 640px)',
+      md: 'screen and (max-width: 768px)',
+      lg: 'screen and (max-width: 1024px)',
+      xl: 'screen and (max-width: 1280px)'
+    },
+  }
